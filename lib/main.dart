82ca63008f5f444cd90a9e7dabb98d1f5829a8c1
ynaivasor <124240128@student.upnyk.ac.id>
2026-09-05 21:11:35 +0700

    membuat fitur hitung calorie

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pages/calorie_page.dart'; 
+
 
 void main() {
   runApp(const MyApp());
@@ -28,9 +30,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),      ),
+      home: CaloriePage(),
     );
   }
 }
